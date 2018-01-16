984a69006b2b2016c816e0b18622321309160872
Sergey Navka <valeriyaponamarenko@WhiteRoses-MacBook-Pro.local>
2018-01-16 22:24:42 +0200

    rename project

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Hello",
+    name: "Vapor-test-example",
     products: [
         .library(name: "App", targets: ["App"]),
         .executable(name: "Run", targets: ["Run"])
